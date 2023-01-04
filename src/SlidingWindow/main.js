fcde317ec0fe2c2b9877a22023c5088778ed91ec
Eduardo Martinez <e.martinez.tamez@gmail.com>
2023-01-04 20:10:42 +0000

    SlidingWindow

---
+++ src/SlidingWindow/main.js
@@ -0,0 +1,63 @@
+export default class SlidingWindow {
+  // write a function called maxSubarraySum which accepts an
+  // array of integers and a number called n. the function should
+  // calculate the maximum sum of n consecutive elements in the array.
+
+  constructor() {
+    console.log(
+      "SlidingWindow",
+      "maxSubarraySum([1,2,5,2,8,1,5], 2) => 10",
+      "result: " + this.maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)
+    );
+    console.log(
+      "SlidingWindow",
+      "maxSubarraySum([1,2,5,2,8,1,5], 4) => 17",
+      "result: " + this.maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)
+    );
+    console.log(
+      "SlidingWindow",
+      "maxSubarraySum([4,2,1,6], 1) => 6",
+      "result: " + this.maxSubarraySum([4, 2, 1, 6], 1)
+    );
+    console.log(
+      "SlidingWindow",
+      "maxSubarraySum([4,2,1,6,2], 4) => 13",
+      "result: " + this.maxSubarraySum([4, 2, 1, 6, 2], 4)
+    );
+    console.log(
+      "SlidingWindow",
+      "maxSubarraySum([], 4) => null",
+      "result: " + this.maxSubarraySum([], 4)
+    );
+  }
+
+  maxSubarraySum(arr1 = [], maxLength = 0) {
+    let leftPointer = 0;
+    let rightPointer = 1;
+    let maxSum = 0;
+    let tempMaxSum = 0;
+    let counter = 1;
+    // iterate for each element as window aproach
+    // keep max sum
+    // if counter is equal to maxlength start again keeping maxSum to compare with the next values
+    if (arr1.length < maxLength) {
+      return;
+    }
+    tempMaxSum = arr1[leftPointer];
+    while (leftPointer <= arr1.length) {
+      if (counter === maxLength) {
+        if (tempMaxSum > maxSum) {
+          maxSum = tempMaxSum;
+        }
+        counter = 0;
+        leftPointer++;
+        rightPointer = leftPointer;
+      } else {
+        tempMaxSum = tempMaxSum + arr1[rightPointer];
+        rightPointer++;
+      }
+    }
+
+    return;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import FrequencyCounterPattern from "./FrequencyCounterPattern/FrequencyCounterPattern";
-import FrequencyCounterPatternAnagrams from "./FrequencyCounterPattern/FrequencyCounterPatternAnagrams";
-import MultiplePointersPattern from "./MultiplePointersPattern/MultiplePointersPattern";
-import MultiplePointersCountUniqueValuesChallenge from "./MultiplePointersPattern/MultiplePointers-CountUniqueValuesChallenge";
+import FrequencyCounterPattern from "./FrequencyCounterPattern/main";
+import FrequencyCounterPatternAnagrams from "./FrequencyCounterPattern/Anagrams";
+import MultiplePointersPattern from "./MultiplePointersPattern/main";
+import MultiplePointersCountUniqueValuesChallenge from "./MultiplePointersPattern/CountUniqueValuesChallenge";
+import SlidingWindow from "./SlidingWindow/main";
 
 function main() {
   // new FrequencyCounterPattern();
   // new FrequencyCounterPatternAnagrams();
   // new MultiplePointersPattern();
-  new MultiplePointersCountUniqueValuesChallenge();
+  // new MultiplePointersCountUniqueValuesChallenge();
+  new SlidingWindow();
 }
 
 main();
